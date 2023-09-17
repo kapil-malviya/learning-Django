69bd70c2e712d2f05e746449dcabe56936dde814
kapil-malviya <etc.kapil@gmail.com>
2023-09-17 16:38:15 +0530

    database based caching particular view

---
+++ cacheview1/testapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+
+# Create your views here.
+
+@cache_page(50)
+def home(request):
+	return render(request, 'testapp/course.html')
+
+def contact(request) :
+	return render(request, 'testapp/contact.html')
